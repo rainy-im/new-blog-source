cfb9d597ab7aff7c800badeae57430a03debab1b
rainy-im <sniky.hack@gmail.com>
2014-12-05 13:42:40 +0800

    theme update

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,7 +13,9 @@ TIMEZONE = 'Asia/Shanghai'
 DEFAULT_LANG = u'zh-CN'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+FEED_MAX_ITEMS = 20
+
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
@@ -35,7 +37,14 @@ THEME = "themes/MD"
 
 DEFAULT_DATE_FORMAT = '%Y-%m-%d'
 
+
 ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
 ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+
+TEMPLATE_PAGES = {"/Users/rainy/Projects/Blog/pelican.rainy.im/themes/MD/templates/about.html": 'about/index.html'}
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Plugins
+PLUGIN_PATH = "plugins"
+PLUGINS = ["neighbors"]
